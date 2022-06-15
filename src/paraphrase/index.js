dab83cd8572dae55f53a68fb58b9d29561859fe1
asafam <13185899+asafam@users.noreply.github.com>
2022-06-15 13:01:05 -0400

    Build

--- src/paraphrase/index.js
+++ src/paraphrase/index.js
@@ -8,7 +8,7 @@ const getData = (attribute, id = 'root') => {
 };
 
 const id = getData('data-id');
-const utterance = getData('data-utterance') && getData('data-utterance').replace('__COMMA__', ',');
+const utterance = getData('data-utterance') && getData('data-utterance').replace(/__COMMA__/g, ',');
 const blacklist = getData('data-blacklist') ? getData('data-blacklist').split(':').map(word => word.trim()) : ["<any word is valid>"]
 const maxLength = (getData('data-max-length') && parseInt(getData('data-max-length'))) || 180;
 const minLength = (getData('data-min-length') && parseInt(getData('data-max-length'))) || 10;
